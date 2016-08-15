490edf1975783fc9b53f78886371f78d7eeccd8f
ahyanarizky <ahyana.rizky@gmail.com>
2016-08-15 13:44:19 +0700

    fix math-basic.js input variable

--- javascript/math-basic.js
+++ javascript/math-basic.js
@@ -1,13 +1,13 @@
 //Menghitung operasi pangkat//
 var pangkatAngka = function(x,y) {
 return Math.pow(x,y);}
-pangkatAngka( , );
+pangkatAngka(2,8);
 
 //Menghitung operasi akar//
 var akarKuadrat = function (x) {
   return Math.sqrt(x);
 }
-akarKuadrat();
+akarKuadrat(256);
 
 //Menghitung Hipotenusa//
 var hipotenusa = function (x,y) {
@@ -15,11 +15,11 @@ var hipotenusa = function (x,y) {
   var panjangB = Math.pow(y,2);
   return Math.sqrt( panjangA + panjangB );
 }
-hipotenusa( , );
+hipotenusa(3,4);
 
 //Menghitung Luas Lingkaran//
 var luasLingkaran = function(x) {
   var phi = Math.PI;
   return phi*(Math.pow(x,2));
 }
-luasLingkaran();
+luasLingkaran(15);
